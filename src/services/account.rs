063d9e689ef226d3524dfac9f54f8e5d5b1a49dd
kari <ammr.arf@gmail.com>
2022-08-09 00:05:59 +0800

    add update function

--- src/services/account.rs
+++ src/services/account.rs
@@ -5,7 +5,7 @@ use reqwest::StatusCode;
 use tonic::{Request, Response, Status};
 
 use crate::proto::account::user_server::User;
-use crate::proto::account::{get_user, UserProfileData};
+use crate::proto::account::{get_by_id, update, UserProfileData};
 use crate::services::{error_messages, Result};
 
 use super::util::create_postgrest_client;
@@ -24,10 +24,10 @@ impl UserService {
 
 #[tonic::async_trait]
 impl User for UserService {
-    async fn get_user(
+    async fn get_by_id(
         &self,
-        request: Request<get_user::Request>,
-    ) -> Result<Response<get_user::Response>> {
+        request: Request<get_by_id::Request>,
+    ) -> Result<Response<get_by_id::Response>> {
         let payload = request.into_inner().payload;
 
         match payload {
@@ -48,7 +48,50 @@ impl User for UserService {
                         let values =
                             serde_json::from_value::<Vec<UserProfileData>>(res_data).unwrap();
 
-                        Ok(Response::new(get_user::Response {
+                        Ok(Response::new(get_by_id::Response {
+                            user: values.into_iter().next(),
+                        }))
+                    }
+
+                    StatusCode::BAD_REQUEST => {
+                        Err(Status::invalid_argument(error_messages::INVALID_PAYLOAD))
+                    }
+
+                    _ => Err(Status::unknown(error_messages::UNKNOWN)),
+                }
+            }
+            _ => Err(Status::invalid_argument(error_messages::INVALID_PAYLOAD)),
+        }
+    }
+
+    async fn update(
+        &self,
+        request: Request<update::Request>,
+    ) -> Result<Response<update::Response>> {
+        let payload = request.into_inner().payload;
+
+        match payload {
+            Some(payload) if !payload.user_id.is_empty() => {
+                let update::Payload { update, user_id } = payload;
+
+                let res = self
+                    .db_client
+                    .from("user_profile")
+                    .eq("user_id", user_id)
+                    .update(update)
+                    .execute()
+                    .await
+                    .unwrap();
+
+                let res_status = res.status();
+                let res_data = res.json::<serde_json::Value>().await.unwrap();
+
+                match res_status {
+                    StatusCode::OK => {
+                        let values = serde_json::from_value::<Vec<UserProfileData>>(res_data)
+                            .unwrap_or_default();
+
+                        Ok(Response::new(update::Response {
                             user: values.into_iter().next(),
                         }))
                     }
